0dbfd00372cd74f74c71a17fc4408bcf8c911991
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-01-15 23:39:46 +0300

    Add files via upload

---
+++ StringOrnegi/StringOrnegi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = new string[5] { "Tokat" , "Erzincan", "Istanbul", "Eskisehir","Amasya" };
+            foreach(string eleman in sehirler)
+            {
+                Console.WriteLine("{0}", eleman);
+             }
+            Console.ReadKey();
+        }
+    }
+}
