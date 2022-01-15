9492a62cab89208073cfee13579b2ff864b3f040
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-01-16 00:07:28 +0300

    Add files via upload

---
+++ StringDiziOrnegi/StringDiziOrnegi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDiziOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = new string[5] { "Tokat", "Erzincan", "Istanbul", "Eskisehir", "Amasya" };
+            Array.Sort(sehirler); // Diziyi alfabetik olarak sıralayıp yeniden oluşturdu.
+            foreach (string eleman in sehirler)
+            {
+                Console.WriteLine("{0}", eleman);
+            }
+            Console.WriteLine("Dizideki Eleman Sayısı: {0}", sehirler.Length); // Dizinin boyutuna/uzunluğuna bakıp kaç elemanlı olduğunu buldu.
+            Console.ReadKey();
+        }
+    }
+}
