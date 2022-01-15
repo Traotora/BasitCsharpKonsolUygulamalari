81c1048c5ee0a4265b115dfefbee72c1db703ed4
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-01-16 00:54:10 +0300

    Add files via upload

---
+++ DiziyiTerstenYazmaOrnegi/DiziyiTerstenYazmaOrnegi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziyiTerstenYazmaOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[5]{1, 2, 3, 4, 5};
+            Array.Reverse(sayilar); // Reverse olmasa yukarıdan aşağıya 1-2-3-4-5 diye sıralanırdı.
+            foreach(int eleman in sayilar)
+            {
+                Console.WriteLine("{0}", eleman);
+            }
+            Console.ReadKey();
+        }
+    }
+}
