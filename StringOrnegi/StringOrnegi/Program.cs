db79c222de6953ee558087a40fe77de15e89e849
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-01-15 23:47:34 +0300

    Update Program.cs

--- StringOrnegi/StringOrnegi/Program.cs
+++ StringOrnegi/StringOrnegi/Program.cs
@@ -9,7 +9,8 @@ namespace StringOrnegi
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // tr: Basit bir string dizisi örneği. / en: An example of a simple string array.
             string[] sehirler = new string[5] { "Tokat" , "Erzincan", "Istanbul", "Eskisehir","Amasya" };
             foreach(string eleman in sehirler)
             {
